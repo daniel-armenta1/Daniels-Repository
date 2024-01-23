7cb94ead55ad4f8dfbeab752434cab7f03c467d9
daniel-armenta1 <157466798+daniel-armenta1@users.noreply.github.com>
2024-01-23 01:24:02 -0800

    Add files via upload

---
+++ Hmwk/Assignment3Probs/Assign3Prob8/main.cpp
@@ -0,0 +1,22 @@
+/* problem 8
+ * File:   main.cpp
+ * Author: Daniel Armenta
+ * Created on January 17 2024
+ * Purpose:  Creation of Template to be used for all
+ *           future projects
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    string ""
+    
+    cout << "Horoscope program which examines compatible signs.\n";
+    cout << "Input 2 signs: ";
+    
+}    return 0;
+
+
